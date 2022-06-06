db42ade2d73e90a2e6dd86c15499b2c88a5fba1f
shahrukh-mujadidia <shahrukh.mushtaq@mujadidia.com>
2022-06-06 21:35:28 +0500

    innitial commit:

---
+++ app.py
@@ -0,0 +1,22 @@
+print("Shahrukh")
+# print("Saylani Batch 1")
+print("Timing 9 to 12")
+print("Location: Gulshan")
+
+age = 1
+
+# if age >= 1 and age < 8:
+#     print("Bachaa")
+# elif age >= 8 and age < 19:
+#     print("Teenager")
+# else:
+#     print("Admi")
+
+# fruits = ["Mangoe", "Peach", "Apple"]
+# for i in range(0, len(fruits)):
+#     print(i)
+#     print(fruits[i])
+
+# [1,2,3,4,5]
+for i in range(1,6):
+    print(int(5-i) * " " +  i * "*")
